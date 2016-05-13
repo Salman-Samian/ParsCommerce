c9674837ccbb80021c43c7081b04cd85a3450711
SamianiLaptop <SamianiLaptop@SAMIANI-LAPTOP>
2016-05-13 16:18:50 +0430

    Add project files.

---
+++ ParsCommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ParsCommerce.Startup))]
+namespace ParsCommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
